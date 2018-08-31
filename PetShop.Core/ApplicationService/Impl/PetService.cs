1aa99b1c56b0a773b91f26e6d0ba0a3a76346a25
Riis18 <Riisjesper@hotmail.com>
2018-08-31 11:25:25 +0200

    Test

--- PetShop.Core/ApplicationService/Impl/PetService.cs
+++ PetShop.Core/ApplicationService/Impl/PetService.cs
@@ -23,6 +23,8 @@ namespace PetShop.Core.ApplicationService.Impl
             {
                 Name = name,
                 Type = type,
+
+
                 Birthdate = birthdate,
                 SoldDate = soldDate,
                 Color = color,
